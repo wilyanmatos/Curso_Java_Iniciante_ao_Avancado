9e47d7858e55eccb9604f3d7ea3b8bf4d093ae9e
wilyan <92753649+wilyanmatos@users.noreply.github.com>
2024-04-08 10:27:41 -0300

    Update aula_07_arrays.java

--- src/Java/iniciante/aula_07_arrays.java
+++ src/Java/iniciante/aula_07_arrays.java
@@ -2,10 +2,47 @@ package Java.iniciante;
 
 public class aula_07_arrays {
     public static void main(String[] args) {
-        int[] idades = new int[3];
-        idades[0] = 21;
-        idades[1] = 15;
-        idades[2] = 11;
-        System.out.println(idades[1]);
+        String[] nomes = new String[3];
+        nomes[0] = "Goku";
+        nomes[1] = "Kurosaki";
+        nomes[2] = "Luffy";
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+        nomes = new String[3];
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+        // Forma diferente de definir e imprimir os arrays
+        int[] numeros = new int[3];
+        int[] numeros2 = {1,2,3,4,5};
+        /*for (int i = 0; i < nomes.length ; i++) {
+            String[] novo_nomes = {nomes[i]};
+            System.out.println(novo_nomes[i]);
+        }*/
+        // foreach
+        /*for(int num: numeros2){
+            System.out.println(num);
+        }
+        */
+        // array multidimensional
+        int lista[][] = new int[3][3];
+        lista[0][0] = 1;
+        lista[0][1] = 2;
+        lista[0][2] = 3;
+
+        lista[1][0] = 4;
+        lista[1][1] = 5;
+        lista[1][2] = 6;
+
+        lista[2][0] = 7;
+        lista[2][1] = 8;
+        lista[2][2] = 9;
+        for (int i = 0; i < lista.length; i++) {
+            for (int j = 0; j < lista[i].length; j++) {
+                System.out.println(lista[i][j]);
+            }
+        }
     }
+
 }
